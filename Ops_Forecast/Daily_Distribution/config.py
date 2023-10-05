012bf5f4c1568b9a1444d8e039d8cfa91fd8b6e5
Mehrdad <45327329+MehrdadDS@users.noreply.github.com>
2023-10-05 09:03:04 -0400

    Update config.py

--- Ops_Forecast/Daily_Distribution/config.py
+++ Ops_Forecast/Daily_Distribution/config.py
@@ -1,2 +1,2 @@
-root_folder = 'C:\My Folder\Python Projects\General files'
-database_url = 'mysql+pymysql://root:123456@localhost/forecasting_schema'
+root_folder = 'C:\My Folder\Python Projects\General files'  # functions.py folder
+database_url = 'mysql+pymysql://root:123456@localhost/forecasting_schema' # for SQL functions
