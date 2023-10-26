8a333ee4573856b08bcd16c7a330e16f8a4be20c
Mehrdad <45327329+MehrdadDS@users.noreply.github.com>
2023-10-26 13:21:46 -0400

    Delete Ops_Forecast/config.py

--- Ops_Forecast/config.py
+++
@@ -1,6 +0,0 @@
-dropped_columns = ['idFMR','Division', 'District','Terminal Name','T_ID-Weekday','Date-Province','Province','Year-Week Number-Province','Working Days Per Period',]
-non_variable_columns = ['CalendarDate', 'Terminal','Year', 'Week Number', 'Period Number','Quarter','Working Days','Week Day','Day Number','Weekend','Holiday']
-variable_columns = ['PCL Del Pcs', 'PCL Del Stops', 'PCL PU Pcs','PCL PU Stops', 'Agent Del Pcs', 'Total Del Stops', 'Agent PU Pcs',
-       'Total PU Stops', 'Courier AM Hours', 'Courier Delivery Hours','Courier Pickup Hours', 'Courier (Del+PU) Hours', 'Courier PM Hours',
-       'Courier Other Hours', 'Couriers/Day', 'Kilometers', 'AM Dock Hours','PM Dock Hours', 'Linehaul Hours', 'SPH', 'AM PPH', 'PM PPH']
-
